b9a69dfefae458e864962a236854e82e749bd546
netanelHAsgardSystems <netanel.h@asgardsys.co.il>
2022-06-15 13:47:36 +0300

    installation-complete

---
+++ build/src/firebaseConfig.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("firebase/app");
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+//...
+};
+const app = (0, app_1.initializeApp)(firebaseConfig);
